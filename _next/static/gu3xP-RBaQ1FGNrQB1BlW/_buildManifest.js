51c8c2f02246e5cb34ba534170075f8809918c6d
genetive <sirisak_mai@hotmail.com>
2022-06-09 22:22:32 +0700

    Add out folder

---
+++ _next/static/gu3xP-RBaQ1FGNrQB1BlW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-187bce3cd72ee9ff.js"],"/_error":["static/chunks/pages/_error-98e801c646abf33c.js"],"/about":["static/chunks/pages/about-387df7f91319affa.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
